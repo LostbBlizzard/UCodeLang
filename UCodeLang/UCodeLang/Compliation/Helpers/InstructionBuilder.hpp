7d102ac1a2dbdfde55d82ed79dc3b9bc9303b6b1
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2022-12-22 11:42:13 -0500

    Fixed Up  BuilderIntSet

--- UCodeLang/UCodeLang/Compliation/Helpers/InstructionBuilder.hpp
+++ UCodeLang/UCodeLang/Compliation/Helpers/InstructionBuilder.hpp
@@ -2,74 +2,77 @@
 #include "../../LangCore.hpp"
 UCodeLangStart
 
-#define BuilderIntSet(bitsize) \
-UCodeLangForceinline static void Store##bitsize(Instruction& Out, RegisterID reg, Int8 Value) \
+
+#pragma region MyRegion
+
+#define BuilderIntSet(bitsize,signedCType,unsignedCType,signedAnyIntValue,unsignedAnyIntValue) \
+UCodeLangForceinline static void Store##bitsize(Instruction& Out, RegisterID reg, signedCType Value) \
 { \
-	Store8(Out, reg, *(UInt8*)&Value); \
+	Store##bitsize(Out, reg, *(unsignedCType*)&Value); \
 }\
-UCodeLangForceinline static void Store8(Instruction& Out, RegisterID reg, UInt8 Value)\
+UCodeLangForceinline static void Store##bitsize(Instruction& Out, RegisterID reg, unsignedCType Value)\
 {\
-	Out.OpCode = InstructionSet::Store8;\
+	Out.OpCode = InstructionSet::Store##bitsize;\
 	Out.Value0.AsRegister = reg;\
-	Out.Value1.AsInt8 = Value;\
+	Out.Value1.##unsignedAnyIntValue = Value;\
 }\
-UCodeLangForceinline static void Store8FromPtrToReg(Instruction& Out, RegisterID pointer, RegisterID OutReg)\
+UCodeLangForceinline static void StoreFromPtrToReg##bitsize(Instruction& Out, RegisterID pointer, RegisterID OutReg)\
 {\
-	Out.OpCode = InstructionSet::Store8FromPtrToReg;\
+	Out.OpCode = InstructionSet::StoreFromPtrToReg##bitsize;\
 	Out.Value0.AsRegister = pointer;\
 	Out.Value1.AsRegister = OutReg;\
 }\
-UCodeLangForceinline static void Store8RegToPtr(Instruction& Out, RegisterID Reg, RegisterID pointer)\
+UCodeLangForceinline static void StoreRegToPtr##bitsize(Instruction& Out, RegisterID Reg, RegisterID pointer)\
 {\
-	Out.OpCode = InstructionSet::Store8RegToPtr;\
+	Out.OpCode = InstructionSet::StoreRegToPtr##bitsize;\
 	Out.Value0.AsRegister = Reg;\
 	Out.Value1.AsRegister = pointer;\
 }\
-UCodeLangForceinline static void Store8RegToReg(Instruction& Out, RegisterID Reg, RegisterID OutReg)\
+UCodeLangForceinline static void StoreRegToReg##bitsize(Instruction& Out, RegisterID Reg, RegisterID OutReg)\
 {\
-	Out.OpCode = InstructionSet::Store8RegToReg;\
+	Out.OpCode = InstructionSet::StoreRegToReg##bitsize;\
 	Out.Value0.AsRegister = Reg;\
 	Out.Value1.AsRegister = OutReg;\
 }\
-UCodeLangForceinline static void Push8(Instruction& Out, RegisterID Reg)\
+UCodeLangForceinline static void Push##bitsize(Instruction& Out, RegisterID Reg)\
 {\
-	Out.OpCode = InstructionSet::Push8;\
-	Out.Value0.AsRegister = Reg;\
-	Out.Value1.AsInt8 = NullUInt64;\
-}\
-UCodeLangForceinline static void Pop8(Instruction& Out, RegisterID Reg)\
-{\
-	Out.OpCode = InstructionSet::Pop8;\
+	Out.OpCode = InstructionSet::Push##bitsize;\
 	Out.Value0.AsRegister = Reg;\
 	Out.Value1.AsInt64 = NullUInt64;\
 }\
-UCodeLangForceinline static void Store8RegOnStack(Instruction& Out, RegisterID Reg, UAddress Stackoffset)\
+UCodeLangForceinline static void Pop##bitsize(Instruction& Out, RegisterID Reg)\
 {\
-	Out.OpCode = InstructionSet::Store8RegOnStack;\
+	Out.OpCode = InstructionSet::Pop##bitsize;\
+	Out.Value0.AsRegister = Reg;\
+	Out.Value1.AsInt64 = NullUInt64;\
+}\
+UCodeLangForceinline static void StoreRegOnStack##bitsize(Instruction& Out, RegisterID Reg, UAddress Stackoffset)\
+{\
+	Out.OpCode = InstructionSet::StoreRegOnStack##bitsize;\
 	Out.Value0.AsRegister = Reg;\
 	Out.Value1.AsAddress = Stackoffset;\
 }\
-UCodeLangForceinline static void Get8FromStack(Instruction& Out, UAddress Stackoffset, RegisterID Reg)\
+UCodeLangForceinline static void GetFromStack##bitsize(Instruction& Out, UAddress Stackoffset, RegisterID Reg)\
 {\
-	Out.OpCode = InstructionSet::Get8FromStack;\
+	Out.OpCode = InstructionSet::GetFromStack##bitsize;\
 	Out.Value0.AsAddress = Stackoffset;\
 	Out.Value1.AsRegister = Reg;\
 }\
-UCodeLangForceinline static void Get8FromStackSub(Instruction& Out, UAddress Stackoffset, RegisterID Reg)\
+UCodeLangForceinline static void GetFromStackSub##bitsize(Instruction& Out, UAddress Stackoffset, RegisterID Reg)\
 {\
-	Out.OpCode = InstructionSet::Get8FromStackSub;\
+	Out.OpCode = InstructionSet::GetFromStackSub##bitsize;\
 	Out.Value0.AsAddress = Stackoffset;\
 	Out.Value1.AsRegister = Reg;\
 }\
-UCodeLangForceinline static void Add8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void Add(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::Add8;\
+	Out.OpCode = InstructionSet::Add##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void Sub8U(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void Sub(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::Sub8;\
+	Out.OpCode = InstructionSet::Sub##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
@@ -103,90 +106,92 @@ UCodeLangForceinline static void LogicalAnd8(Instruction& Out, RegisterID reg, R
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void LogicalOr8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void LogicalOr##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::Logicalor8;\
+	Out.OpCode = InstructionSet::Logicalor##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void LogicalNot8(Instruction& Out, RegisterID reg, RegisterID out)\
+UCodeLangForceinline static void LogicalNot##bitsize(Instruction& Out, RegisterID reg, RegisterID out)\
 {\
-	Out.OpCode = InstructionSet::LogicalNot8;\
+	Out.OpCode = InstructionSet::LogicalNot##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = out;\
 }\
-UCodeLangForceinline static void equalto8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void equalto##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::equalto8;\
+	Out.OpCode = InstructionSet::equalto##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void notequalto8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void notequalto##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::notequalto8;\
+	Out.OpCode = InstructionSet::notequalto##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void lessthan8(Instruction& Out, RegisterID reg, RegisterID reg2)\
-{ /
-	Out.OpCode = InstructionSet::lessthan8; \
+UCodeLangForceinline static void lessthan##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
+{ \
+	Out.OpCode = InstructionSet::lessthan##bitsize; \
 	Out.Value0.AsRegister = reg; \
 	Out.Value1.AsRegister = reg2; \
 }\
-UCodeLangForceinline static void greaterthan8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void greaterthan##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::greaterthan8;\
+	Out.OpCode = InstructionSet::greaterthan##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void equal_lessthan8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void equal_lessthan##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::equal_lessthan8;\
+	Out.OpCode = InstructionSet::equal_lessthan##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void equal_greaterthan8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void equal_greaterthan##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::equal_greaterthan8;\
+	Out.OpCode = InstructionSet::equal_greaterthan##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void bitwise_and8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void bitwise_and##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::bitwiseAnd8;\
+	Out.OpCode = InstructionSet::bitwiseAnd##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void bitwise_or8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void bitwise_or##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::bitwiseOr;\
+	Out.OpCode = InstructionSet::bitwiseOr##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void bitwise_LeftShift8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void bitwise_LeftShift##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::bitwiseLeftShift8;\
+	Out.OpCode = InstructionSet::bitwiseLeftShift##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void bitwise_RightShift8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void bitwise_RightShift##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::bitwiseRightShift8;\
+	Out.OpCode = InstructionSet::bitwiseRightShift##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void bitwise_Xor8(Instruction& Out, RegisterID reg, RegisterID reg2)\
+UCodeLangForceinline static void bitwise_Xor##bitsize(Instruction& Out, RegisterID reg, RegisterID reg2)\
 {\
-	Out.OpCode = InstructionSet::bitwiseXor8;\
+	Out.OpCode = InstructionSet::bitwiseXor##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = reg2;\
 }\
-UCodeLangForceinline static void bitwise_Not8(Instruction& Out, RegisterID reg, RegisterID out)\
+UCodeLangForceinline static void bitwise_Not##bitsize(Instruction& Out, RegisterID reg, RegisterID out)\
 {\
-	Out.OpCode = InstructionSet::bitwise_Not8;\
+	Out.OpCode = InstructionSet::bitwise_Not##bitsize;\
 	Out.Value0.AsRegister = reg;\
 	Out.Value1.AsRegister = out;\
 }\
+
+#pragma endregion
 	
 class InstructionBuilder
 {
@@ -261,16 +266,16 @@ public:
 	}
 
 	//set 8
-	BuilderIntSet(8)
+	BuilderIntSet(8,Int8,UInt8, AsInt8, AsUInt8)
 	//set 16 
-	BuilderIntSet(16)
+	BuilderIntSet(16, Int16, UInt16, AsInt16, AsUInt16)
 	//set 32 
-	BuilderIntSet(32)
+	BuilderIntSet(32, Int32, UInt32, AsInt32, AsUInt32)
 	//set 64 
-	BuilderIntSet(64)
+	BuilderIntSet(64, Int64, UInt64, AsInt64, AsUInt64)
 	
 	//Native Set
-	BuilderIntSet(Native)
+	BuilderIntSet(Native, SIntNative, UIntNative, AsInt64, AsUInt64)
 	//C funcs
 	UCodeLangForceinline static void GetPointerOfStack(Instruction& Out, RegisterID out)
 	{
