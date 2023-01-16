488a88e90570bce98861771716499c314482b37d
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-01-16 17:18:01 -0500

    tryed adding beter CPPCalls

--- UCodeLang/UCodeLang/RunTime/RunTimeLib.hpp
+++ UCodeLang/UCodeLang/RunTime/RunTimeLib.hpp
@@ -31,6 +31,86 @@ public:
 		_NameToCppCall[Name] = CPP;
 	}
 	
+
+
+	template<size_t V,typename... Pars>
+	void Add_CPPCall(const String& Name,void(*CPP)(Pars...) )
+	{
+		using FuncType = void(*)(Pars...);
+		const static thread_local FuncType   _Cpp = CPP;
+		struct CPPFunc
+		{
+			static void UCodeLangAPI Invoke(InterpreterCPPinterface& interpreter)
+			{
+				Pars... Par;
+				interpreter.GetParameters(Par);
+				_Cpp(Par);
+				interpreter.Set_Return();
+			}
+		};
+		Add_CPPCall(Name, CPPFunc::Invoke);
+	}
+
+	template<size_t V,typename T, typename... Pars>
+	void Add_CPPCall(const String& Name, T(*CPP)(Pars...) )
+	{
+		using FuncType = T(*)(Pars...);
+		const static thread_local FuncType   _Cpp = CPP;
+		struct CPPFunc
+		{
+			static void UCodeLangAPI Invoke(InterpreterCPPinterface& interpreter)
+			{
+				Pars... Par;
+				interpreter.GetParameters(Par);
+				interpreter.Set_Return(_Cpp(Par));
+			}
+		};
+		Add_CPPCall(Name, CPPFunc::Invoke);
+	}
+
+
+	/*
+	template<typename This,typename... Pars>
+	void Add_CPPCall(const String& Name, void(This::*CPP)(Pars...))
+	{
+		using FuncType = void(This::*)(Pars...);
+		const static thread_local FuncType   _Cpp = CPP;
+		struct CPPFunc
+		{
+			static void UCodeLangAPI Invoke(InterpreterCPPinterface& interpreter)
+			{
+				This* _This = interpreter.GetParameter<This*>();
+				Pars... Par;
+				interpreter.GetParameters(std::forward<Pars>(Par)...);
+
+				(_This->*_Cpp)(Par);
+
+				interpreter.Set_Return();
+			}
+		};
+		Add_CPPCall(Name, CPPFunc::Invoke);
+	}
+
+	template<typename R, typename This,typename... Pars> 
+	void Add_CPPCall(const String& Name, R(This::* CPP)(Pars...))
+	{
+		using FuncType = R(This::*)(Pars...);
+		//const static thread_local FuncType _Cpp = CPP;
+		struct CPPFunc
+		{
+			static void UCodeLangAPI Invoke(InterpreterCPPinterface& interpreter)
+			{
+				This* _This = interpreter.GetParameter<This*>();
+				Pars... Par;
+				 interpreter.GetParameters(Par);
+
+				//interpreter.Set_Return((_This->*_Cpp)(Par));
+			}
+		};
+		Add_CPPCall(Name, CPPFunc::Invoke);
+	}
+	*/
+
 private:
 	UClib* _Lib;
 	Vector<Instruction> _Instruction;
