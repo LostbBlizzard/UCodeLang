95f1658d314d499ea3a5a51ab1c5450915b66279
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-02-02 19:46:47 -0500

    added SyntaxHighlighter

---
+++ UCodeLang/UCodeAnalyzer/Intellisense/SyntaxHighlighter.cpp
@@ -0,0 +1,27 @@
+#include "SyntaxHighlighter.hpp"
+UCodeAnalyzerStart
+
+using UToken = UCodeLang::Token;
+using UTokenType = UCodeLang::TokenType;
+bool IsKeyWord(UTokenType T)
+{
+
+}
+
+void SyntaxHighlighter::Parse(StringView Text)
+{
+	if (_Data == nullptr) { return; }
+	auto& Lexer = _Data->Get_Lexer();
+
+	_Tokens.clear();
+	
+
+	Lexer.Lex(Text);
+	const auto& Output = Lexer.Get_Tokens();
+
+	for (auto& Item : Output)
+	{
+		
+	}
+}
+UCodeAnalyzerEnd
