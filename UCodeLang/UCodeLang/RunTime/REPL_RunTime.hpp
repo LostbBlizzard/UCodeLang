79fbcac2f573b6d579b34eac4f3146cb321d10aa
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-05-25 18:46:11 -0400

    added REPL_RunTime

---
+++ UCodeLang/UCodeLang/RunTime/REPL_RunTime.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include "Interpreters/Interpreter.hpp"
+#include "../Compliation/Compiler.hpp"
+UCodeLangStart
+class REPL_RunTime
+{
+public:
+	REPL_RunTime()
+	{
+
+	}
+	~REPL_RunTime()
+	{
+
+	}
+	void Set_Compiler(Compiler* Compiler)
+	{
+		_Compiler = Compiler;
+	}
+	void Set_Interpreter(Interpreter* Interpreter)
+	{
+		_Interpreter = Interpreter;
+	}
+	
+
+	void ReadLine(String_view Text);
+
+private:
+	Compiler* _Compiler =nullptr;
+	Interpreter* _Interpreter =nullptr;
+};
+
+UCodeLangEnd
