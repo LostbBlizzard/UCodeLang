32c6a3ae78ef3feadb693402c5c618a3695644df
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-06-05 20:04:16 -0400

    update reflectiondata

--- UCodeLang/UCodeLang/LangCore/ReflectionData.hpp
+++ UCodeLang/UCodeLang/LangCore/ReflectionData.hpp
@@ -94,10 +94,14 @@ public:
 	ReflectionTypeInfo Type;
 	size_t offset=NullAddress;
 };
-class AttributeData
+class UsedTagValueData
 {
 public:
 	String Name;
+	~UsedTagValueData()
+	{
+
+	}
 };
 class ClassMethod
 {
@@ -109,22 +113,46 @@ public:
 	ReflectionTypeInfo RetType;
 	Vector<ReflectionTypeInfo> ParsType;
 
-	Vector<AttributeData> Attributes;
+	Vector<UsedTagValueData> Attributes;
 };
+
+class ReflectionRawData
+{
+public:
+	size_t Size = 0;
+	Unique_Array<Byte> Bytes;
+
+	inline void Resize(size_t size)
+	{
+		Bytes.reset(new Byte[size]);
+		Size = size;
+	}
+	inline void* Get_Data()
+	{
+		return Bytes.get();
+	}
+	inline const void* Get_Data() const
+	{
+		return Bytes.get();
+	}
+};
+
+class TypedRawReflectionData
+{
+public:
+	ReflectionTypeInfo _Type;
+	ReflectionRawData _Data;
+};
+
 class EnumValues
 {
 public:
-	using State_t = UInt8;
-	enum class State : State_t
-	{
-		Ok,
-		Bad,
-	};
 	String Name;
-	State _State;
-	size_t Value;
-};
+	ReflectionRawData _Data;
 
+	//The Type the Enum Variant Value  is associated with.
+	Optional<ReflectionTypeInfo> EnumVariantType;
+};
 
 using ClassType_t = UInt8;
 enum class ClassType :ClassType_t
@@ -133,132 +161,324 @@ enum class ClassType :ClassType_t
 	Class,
 	Enum,
 	Alias,
+	Eval,
+	Tag,
+	Trait,
+	StaticVarable,
+	ThreadVarable,
+	StaticArray,
+	FuncPtr,
+	GenericClass,
+	GenericFuncion,
 };
-using EnumSizez_t = UInt8;
-class EnumSizez
+struct Class_Data
 {
-public:
-	enum : EnumSizez_t
+	ReflectionCustomTypeID TypeID;
+	size_t Size = 0;
+	Vector<AttributeData> Attributes;
+	Vector<ClassField> Fields;
+	Vector<ClassMethod> Methods;
+	inline bool HasAttribute(const String& Name)
 	{
-		Null = 0,
-		int8,
-		int16,
-		int32,
-		int64,
-	};
-	inline static EnumSizez FromSize(size_t V)
-	{
-		switch (V)
+		for (const auto& Item : Attributes)
 		{
-		case sizeof(Int8):return { EnumSizez::int8 };
-		case sizeof(Int16):return { EnumSizez::int16 };
-		case sizeof(Int32):return{ EnumSizez::int32 };
-		case sizeof(Int64):return{ EnumSizez::int64 };
-		default:return{ EnumSizez::Null };
+			if (Item.Name == Name)
+			{
+				return true;
+			}
 		}
+		return true;
 	}
-	EnumSizez_t V = EnumSizez::Null;
+	inline const AttributeData* GetAttribute(const String& Name)
+	{
+		for (const auto& Item : Attributes)
+		{
+			if (Item.Name == Name)
+			{
+				return &Item;
+			}
+		}
+		return nullptr;
+	}
 
-	operator EnumSizez_t() { return V; };
-	EnumSizez(EnumSizez_t Val) :V(Val)
+	const ClassMethod* Get_ClassInit() const
+	{
+		return Get_ClassMethod(ClassInitializefuncName);
+	}//This May be null.
+
+	const ClassMethod* Get_ClassConstructor() const
+	{
+		return Get_ClassMethod(ClassConstructorfunc);
+	}//This May be null.
+
+	const ClassMethod* Get_ClassDestructor() const
+	{
+		return Get_ClassMethod(ClassDestructorFunc);
+	}//This May be null.
+
+	const ClassMethod* Get_ClassMethod(const String& Name) const
+	{
+		for (auto& Item : Methods)
+		{
+			if (ScopeHelper::GetNameFromFullName(Item.FullName)
+				== Name)
+			{
+				return &Item;
+			}
+		}
+		return nullptr;
+	}
+};
+struct Enum_Data
+{
+	ReflectionCustomTypeID TypeID;
+	ReflectionTypeInfo BaseType;
+	Optional<ReflectionCustomTypeID> EnumVariantUnion;//is class
+
+	Vector<EnumValues> Values;
+};
+struct Alias_Data
+{
+	ReflectionTypeInfo Type;
+	Optional<ReflectionCustomTypeID> HardAliasTypeID;
+};
+struct Eval_Data
+{
+	TypedRawReflectionData Value;
+};
+struct Tag_Data
+{
+	~Tag_Data()
 	{
 
 	}
 };
+struct Trait_Data
+{
+	~Trait_Data()
+	{
 
-class ClassData
+	}
+};
+struct StaticVar_Data
+{
+	~StaticVar_Data()
+	{
+
+	}
+};
+struct ThreadVar_Data
+{
+	~ThreadVar_Data()
+	{
+
+	}
+};
+struct StaticArray_Data
+{
+	~StaticArray_Data()
+	{
+
+	}
+};
+struct FuncPtr_Data
+{
+	~FuncPtr_Data()
+	{
+
+	}
+};
+struct GenericClass_Data
+{
+	~GenericClass_Data()
+	{
+
+	}
+};
+struct GenericFuncion_Data
+{
+	~GenericFuncion_Data()
+	{
+
+	}
+};
+class AssemblyNode
 {
 
 public:
-	
-	struct Class_Data
-	{
-		size_t Size = 0;
-		Vector<AttributeData> Attributes;
-		Vector<ClassField> Fields;
-		Vector<ClassMethod> Methods;
-		inline bool HasAttribute(const String& Name)
-		{
-			for (const auto& Item : Attributes)
-			{
-				if (Item.Name == Name)
-				{
-					return true;
-				}
-			}
-			return true;
-		}
-		inline const AttributeData* GetAttribute(const String& Name)
-		{
-			for (const auto& Item : Attributes)
-			{
-				if (Item.Name == Name)
-				{
-					return &Item;
-				}
-			}
-			return nullptr;
-		}
-
-		const ClassMethod* Get_ClassInit() const
-		{
-			return Get_ClassMethod(ClassInitializefuncName);
-		}//This May be null.
-		
-		const ClassMethod* Get_ClassConstructor() const
-		{
-			return Get_ClassMethod(ClassConstructorfunc);
-		}//This May be null.
-
-		const ClassMethod* Get_ClassDestructor() const
-		{
-			return Get_ClassMethod(ClassDestructorFunc);
-		}//This May be null.
-
-		const ClassMethod* Get_ClassMethod(const String& Name) const
-		{
-			for (auto& Item : Methods)
-			{
-				if (ScopeHelper::GetNameFromFullName(Item.FullName)
-					== Name)
-				{
-					return &Item;
-				}
-			}
-			return nullptr;
-		}
-	};
-	struct Enum_Data
-	{
-		EnumSizez Size = { EnumSizez::Null };
-		Vector<EnumValues> Values;
-	};
-	struct Alias_Data
-	{
-		ReflectionTypeInfo Type;
-	};
-	
 	String Name;
 	String FullName;
 	
+	AssemblyNode(ClassType type) : Type(type)
+	{
+		switch (type)
+		{
+		case ClassType::Null:
+			break;
+		case ClassType::Class:
+			_Class = Class_Data();
+			break;
+		case ClassType::Enum:
+			_Enum = Enum_Data();
+			break;
+		case ClassType::Alias:
+			_Alias = Alias_Data();
+			break;
+		case ClassType::Eval:
+			_Eval = Eval_Data();
+			break;
+		case ClassType::Trait:
+			_Trait = Trait_Data();
+			break;
+		case ClassType::Tag:
+			_Tag = Tag_Data();
+			break;
+		case ClassType::StaticVarable:
+			_StaticVar = StaticVar_Data();
+			break;
+		case ClassType::ThreadVarable:
+			_ThreadVar = ThreadVar_Data();
+			break;
+		default:
+			throw std::exception("bad path");
+			break;
+		}
+	}
+	~AssemblyNode()
+	{
+		switch (Type)
+		{
+		case ClassType::Null:
+			break;
+		case ClassType::Class:
+			_Class.~Class_Data();
+			break;
+		case ClassType::Enum:
+			_Enum.~Enum_Data();
+			break;
+		case ClassType::Alias:
+			_Alias.~Alias_Data();
+			break;
+		case ClassType::Eval:
+			_Eval.~Eval_Data();
+			break;
+		case ClassType::Trait:
+			_Trait.~Trait_Data();
+			break;
+		case ClassType::Tag:
+			_Tag.~Tag_Data();
+			break;
+		case ClassType::StaticVarable:
+			_StaticVar.~StaticVar_Data();
+			break;
+		case ClassType::ThreadVarable:
+			_ThreadVar.~ThreadVar_Data();
+			break;
+		default:
+			throw std::exception("bad path");
+			break;
+		}
+	}
+
+	Class_Data& Get_ClassData()
+	{
+		if (Type != ClassType::Class) {throw std::exception("bad access");}
+		return _Class;
+	}
+	Enum_Data& Get_EnumData()
+	{
+		if (Type != ClassType::Enum) { throw std::exception("bad access"); }
+		return _Enum;
+	}
+	Alias_Data& Get_AliasData()
+	{
+		if (Type != ClassType::Alias) { throw std::exception("bad access"); }
+		return _Alias;
+	}
+	Eval_Data& Get_EvalData()
+	{
+		if (Type != ClassType::Eval) { throw std::exception("bad access"); }
+		return  _Eval;
+	}
+	Trait_Data& Get_TraitData()
+	{
+		if (Type != ClassType::Trait) { throw std::exception("bad access"); }
+		return  _Trait;
+	}
+	Tag_Data& Get_TagData()
+	{
+		if (Type != ClassType::Tag) { throw std::exception("bad access"); }
+		return  _Tag;
+	}
+	StaticVar_Data& Get_StaticVar()
+	{
+		if (Type != ClassType::StaticVarable) { throw std::exception("bad access"); }
+		return  _StaticVar;
+	}
+	ThreadVar_Data& Get_ThreadVar()
+	{
+		if (Type != ClassType::ThreadVarable) { throw std::exception("bad access"); }
+		return _ThreadVar;
+	}
+
+	const Class_Data& Get_ClassData() const
+	{
+		if (Type != ClassType::Class) { throw std::exception("bad access"); }
+		return _Class;
+	}
+	const Enum_Data& Get_EnumData()const
+	{
+		if (Type != ClassType::Enum) { throw std::exception("bad access"); }
+		return _Enum;
+	}
+	const Alias_Data& Get_AliasData()const
+	{
+		if (Type != ClassType::Alias) { throw std::exception("bad access"); }
+		return _Alias;
+	}
+	const Eval_Data& Get_EvalData()const
+	{
+		if (Type != ClassType::Eval) { throw std::exception("bad access"); }
+		return  _Eval;
+	}
+	const Trait_Data& Get_TraitData()const
+	{
+		if (Type != ClassType::Trait) { throw std::exception("bad access"); }
+		return  _Trait;
+	}
+	const Tag_Data& Get_TagData()const
+	{
+		if (Type != ClassType::Tag) { throw std::exception("bad access"); }
+		return  _Tag;
+	}
+	const StaticVar_Data& Get_StaticVar()const
+	{
+		if (Type != ClassType::StaticVarable) { throw std::exception("bad access"); }
+		return  _StaticVar;
+	}
+	const ThreadVar_Data& Get_ThreadVar() const
+	{
+		if (Type != ClassType::ThreadVarable) { throw std::exception("bad access"); }
+		return _ThreadVar;
+	}
+	
+	inline ClassType Get_Type() const
+	{
+		return Type;
+	}
+private:
 	ClassType Type = ClassType::Null;
-	//union 
-	struct 
+	union 
 	{
 		Class_Data _Class;
 		Enum_Data _Enum;
 		Alias_Data _Alias;
+		Eval_Data _Eval;
+		Trait_Data _Trait;
+		Tag_Data _Tag;
+		StaticVar_Data _StaticVar;
+		ThreadVar_Data _ThreadVar;
 	};
-	ClassData(ClassType type) : Type(type)
-	{
-		_Class = Class_Data();
-		_Enum = Enum_Data();
-		_Alias = Alias_Data();
-	}
-	~ClassData() 
-	{
-		
-	}
 };
 class ClassAssembly
 {
