4fe96ca031f2de6d05cce68e1db60250cbd6a982
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-07-22 15:36:33 -0400

    updated span and fixed enum on CppHelper

--- UCApp/tests/CppHelperFiles/Files/Func.hpp
+++ UCApp/tests/CppHelperFiles/Files/Func.hpp
@@ -8,6 +8,12 @@ namespace CppLinkTest {
 	//should be an outfunc
 	UCodeLangExportSymbol("UCodeLang") void SomeFuncPtr(UCodeLangOutPartype(int) V) {}
 
+	//This func should have two copys
+	UCodeLangExportSymbol("UCodeLang") void inline SpanTest(UCodeLang::BytesView ) {}
+
+	//should be an outfunc
+	UCodeLangExportSymbol("UCodeLang") void inline SomeFuncPtrWithInline(UCodeLangOutPartype(int) V) {}
+
 	/// <summary>
 	/// Testing
 	/// </summary>
@@ -28,21 +34,34 @@ namespace CppLinkTest {
 	};
 
 	UCodeLangExportSymbol("UCodeLang") constexpr static size_t SomeValue3 = 0;
-	UCodeLangExportSymbol("UCodeLang") constexpr size_t SomeValue4 = 0;
+	UCodeLangExportSymbol("UCodeLang") constexpr bool SomeValue4 = true;
 
 	UCodeLangExportSymbol("UCodeLang") struct SomeClass
 	{
-
 		UCodeLangExport int SomeValue = 0;
-		int NonExportedInt = 0;
+		UCodeLangExclude int NonExportedInt = 0;
 
 		UCodeLangExport constexpr static size_t SomeValue2 = 0;
 
 		//this should be a member funcion
-		UCodeLangExportSymbol("UCodeLang") void SomeMemberFunc(char V) {}
+		UCodeLangExport void SomeMemberFunc(char V) {}
 
 		//this should be a static funcion
-		UCodeLangExportSymbol("UCodeLang") void SomeMemberStatic(char& V) {}
+		UCodeLangExport void SomeMemberStatic(char& V) {}
+		
+		
+		//should make new
+		UCodeLangExport SomeClass()
+		{
+
+		}
+
+		//should make drop
+		UCodeLangExport ~SomeClass()
+		{
+
+		}
+
 	};
 
 	UCodeLangExportSymbol("UCodeLang") void SomeFunc() {}
