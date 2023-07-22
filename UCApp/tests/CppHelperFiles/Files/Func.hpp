f7e74ed7ee3fd50eec21791a70bd6151fc79e1d3
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-07-22 14:35:48 -0400

    fixed CppHelper out cpp file

--- UCApp/tests/CppHelperFiles/Files/Func.hpp
+++ UCApp/tests/CppHelperFiles/Files/Func.hpp
@@ -1,51 +1,53 @@
 #pragma once
 #include "UCodeAnalyzer/Preprocessors/CppHelper.hpp"
 
+namespace CppLinkTest {
+	//This func should have two copys
+	UCodeLangExportSymbol("UCodeLang") void SomeFuncDefault(int V = 5) {}
 
-//This func should have two copys
-UCodeLangExportSymbol("UCodeLang") void SomeFuncDefault(int V = 5) {}
+	//should be an outfunc
+	UCodeLangExportSymbol("UCodeLang") void SomeFuncPtr(UCodeLangOutPartype(int) V) {}
 
-//should be an outfunc
-UCodeLangExportSymbol("UCodeLang") void SomeFuncPtr(UCodeLangOutPartype(int) V) {}
+	/// <summary>
+	/// Testing
+	/// </summary>
+	UCodeLangExportSymbol("UCodeLang") enum class Color : short
+	{
+		SomeValue,
+		OtherValue,
+		YepValue,
+	};
 
-/// <summary>
-/// Testing
-/// </summary>
-UCodeLangExportSymbol("UCodeLang") enum class Color : short
-{
-	SomeValue,
-	OtherValue,
-	YepValue,
-};
+	UCodeLangExportSymbol("UCodeLang") enum Color2 : int
+	{
+		SomeValue = 0,
+		OtherValue = 1,
+		YepValue = 2,
 
-UCodeLangExportSymbol("UCodeLang") enum Color2 : int
-{
-	SomeValue = 0,
-	OtherValue = 1,
-	YepValue = 2,
+		UCodeLangExclude NoneShowedValue,
+	};
 
-	UCodeLangExclude NoneShowedValue,
-};
+	UCodeLangExportSymbol("UCodeLang") constexpr static size_t SomeValue3 = 0;
+	UCodeLangExportSymbol("UCodeLang") constexpr size_t SomeValue4 = 0;
 
-UCodeLangExportSymbol("UCodeLang") constexpr static size_t SomeValue3 = 0;
-UCodeLangExportSymbol("UCodeLang") constexpr size_t SomeValue4 = 0;
+	UCodeLangExportSymbol("UCodeLang") struct SomeClass
+	{
 
-UCodeLangExportSymbol("UCodeLang") struct SomeClass
-{
+		UCodeLangExport int SomeValue = 0;
+		int NonExportedInt = 0;
 
-	UCodeLangExport int SomeValue = 0;
-	int NonExportedInt = 0;
+		UCodeLangExport constexpr static size_t SomeValue2 = 0;
 
-	UCodeLangExport constexpr static size_t SomeValue2 = 0;
+		//this should be a member funcion
+		UCodeLangExportSymbol("UCodeLang") void SomeMemberFunc(char V) {}
 
-	//this should be a member funcion
-	UCodeLangExportSymbol("UCodeLang") void SomeMemberFunc(char V) {}
+		//this should be a static funcion
+		UCodeLangExportSymbol("UCodeLang") void SomeMemberStatic(char& V) {}
+	};
 
-	//this should be a static funcion
-	UCodeLangExportSymbol("UCodeLang") void SomeMemberStatic(char& V) {}
-};
+	UCodeLangExportSymbol("UCodeLang") void SomeFunc() {}
 
-UCodeLangExportSymbol("UCodeLang") void SomeFunc() {}
+	UCodeLangExportSymbol("UCodeLang") using CoolInt = int;
+	UCodeLangExportSymbol("UCodeLang") typedef int CoolInt2;
 
-UCodeLangExportSymbol("UCodeLang") using CoolInt = int;
-UCodeLangExportSymbol("UCodeLang") typedef int CoolInt2;
+}
