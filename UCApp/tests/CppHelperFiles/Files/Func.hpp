d265fd86905d83236ac0cecc3aba15753119f361
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-07-22 22:08:31 -0400

    fixed namespaces

--- UCApp/tests/CppHelperFiles/Files/Func.hpp
+++ UCApp/tests/CppHelperFiles/Files/Func.hpp
@@ -1,17 +1,21 @@
 #pragma once
 #include "UCodeAnalyzer/Preprocessors/CppHelper.hpp"
 
-namespace CppLinkTest {
-
-	
-
+namespace CppLinkTest 
+{
 	UCodeLangExportSymbol("UCodeLang") struct ItemClass
 	{
 		UCodeLangExport void ThisFunc(){}
 		UCodeLangExport void ThisFunc(int H) {}
-		UCodeLangExport void ThisFunc(bool H) {}
+		UCodeLangExport int ThisFunc(bool H) { return 0; }
 
 		UCodeLangExport static void StaticFunc(){}
 
 	};
+
+	//This func should have two copys
+	UCodeLangExportSymbol("UCodeLang") void SomeFuncDefault(int V = 5) {}
+
+	//should be an outfunc
+	UCodeLangExportSymbol("UCodeLang") void SomeFuncPtrWithInline(UCodeLangOutPartype(int) V) {}
 }
