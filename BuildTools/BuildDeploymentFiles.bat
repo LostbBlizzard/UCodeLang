47bf29c081434598ad3ad1937cab1650703f8228
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-07-26 19:14:17 -0400

    added Deployment folder

---
+++ BuildTools/BuildDeploymentFiles.bat
@@ -0,0 +1,29 @@
+
+
+xcopy /y ..\Output\UCodelangCL\Win32\Published\ucodelang.exe ..\Output\Deployment\Runables\Win32\ucodelang.exe*
+xcopy /y ..\Output\UCodeLanguageSever\Win32\Published\UCodeLanguageSever.exe ..\Output\Deployment\Runables\Win32\UCodeLanguageSever.exe*
+xcopy /y ..\Output\UCodeIDE\Win32\Published\UCodeIDE.exe ..\Output\Deployment\Runables\Win32\UCodeIDE.exe*
+xcopy /y ..\Output\UCodeLang\Win32\Published\UCodeLang.lib ..\Output\Deployment\Precompiled\Win32\UCodeLang.lib*
+
+xcopy /y ..\Output\UCodelangCL\Win64\Published\ucodelang.exe ..\Output\Deployment\Runables\Win64\ucodelang.exe*
+xcopy /y ..\Output\UCodeLanguageSever\Win64\Published\UCodeLanguageSever.exe ..\Output\Deployment\Runables\Win64\UCodeLanguageSever.exe*
+xcopy /y ..\Output\UCodeIDE\Win64\Published\UCodeLanguageSever.exe ..\Output\Deployment\Runables\Win64\UCodeIDE.exe*
+xcopy /y ..\Output\UCodeLang\Win64\Published\UCodeLang.lib ..\Output\Deployment\Precompiled\Win64\UCodeLang.lib*
+
+xcopy /y /s/e ..\UCodeAPI\StandardLibrary\ ..\Output\Deployment\ULangSource\StandardLibrary\
+xcopy /y /s/e ..\UCodeAPI\NStandardLibrary\ ..\Output\Deployment\ULangSource\NStandardLibrary\
+xcopy /y /s/e ..\UCodeAPI\NWin32\ ..\Output\Deployment\ULangSource\NWin32\
+xcopy /y /s/e ..\UCodeAPI\Win32\ ..\Output\Deployment\ULangSource\Win32\
+
+cd ..\UCodeDocumentation
+call Build.bat
+xcopy /y /s/e ..\Output\UCodeDocumentation\ ..\Output\Deployment\Documentation\
+cd ..\BuildTools
+
+
+
+pause 
+
+msbuild ../UCodeLang.sln /t:Build /p:Configuration=Published /p:Platform=Win32 -maxcpucount
+
+msbuild ../UCodeLang.sln /t:Build /p:Configuration=Published /p:Platform=Win64 -maxcpucount
--- BuildTools/Deployment.bat
+++
@@ -1,4 +0,0 @@
-
-msbuild ../UCodeLang.sln /t:Build /p:Configuration=Published /p:Platform=Win32 -maxcpucount
-
-msbuild ../UCodeLang.sln /t:Build /p:Configuration=Published /p:Platform=Win64 -maxcpucount
