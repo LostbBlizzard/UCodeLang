983b2e145d8166ddc4955e3dce22a92d3a32fa7f
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-07-26 17:34:10 -0400

    add UCodeIDE Project

---
+++ UCodeIDE/src/main.cpp
@@ -0,0 +1,22 @@
+#include <UCodeLang/LangCore/LangDef.hpp>
+#include "App.hpp"
+
+#ifdef PUBLISHED
+#ifdef  UCodeLang_Platform_Windows
+#include <Windows.h>
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow)
+{
+	LPWSTR* argv;
+	int argc;
+	argv = CommandLineToArgvW(GetCommandLine(), &argc);
+	return App::main(argc, (char**)&argv);
+}
+#endif 
+#else
+
+int main(int argc, char* argv[])
+{
+	return App::main(argc, argv);
+}
+
+#endif 
