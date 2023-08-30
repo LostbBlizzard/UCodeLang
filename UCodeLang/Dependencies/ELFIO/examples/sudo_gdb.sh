d9c5ea496f15cdbc76000c55d5787008a6d7d13b
LostbBlizzard <noodproplay@gmail.com>
2023-08-30 16:44:43 -0400

    removed PE or ELFO to build for linux

--- UCodeLang/Dependencies/ELFIO/examples/sudo_gdb.sh
+++
@@ -1,3 +0,0 @@
-# The file is used for VSCode debugging with sudo. Use it as following:
-#         "miDebuggerPath": "/home/user/ELFIO/examples/sudo_gdb.sh"
-sudo /usr/bin/gdb "$@"
--- UCodeLang/Dependencies/enma_pe/include/pe_highlevel.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "pe_image_strings.h"
-#include "pe_image_rtti.h"
-#include "pe_image_resources_hl.h"
-#include "pe_image_utility.h"
