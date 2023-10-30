d207416db83a0ecab3582214ce1d67c30b53c5fe
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-10-29 23:24:50 -0700

    fixed windows build

--- UCodeLang/UCodeLang/Compliation/Back/ARM64/ARM64JitCompiler.cpp
+++ UCodeLang/UCodeLang/Compliation/Back/ARM64/ARM64JitCompiler.cpp
@@ -7,11 +7,11 @@ void ARM64JitCompiler::Reset()
 }
 bool ARM64JitCompiler::BuildFunc(Vector<Instruction>& Ins, UAddress funcAddress, Vector<UInt8>& ArmOutput)
 {
-
+	return false;
 }
 void ARM64JitCompiler::SubCall(FuncType Value, uintptr_t CPPOffset, void* ArmOutput)
 {
-
+	return false;
 }
 
 UCodeLangEnd
