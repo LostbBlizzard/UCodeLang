d9819d2f449a88c5190e0b01526403b9661250fc
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-10-30 18:41:14 -0700

    updated System file types

--- UCodeLang/UCodeLang/Compliation/Back/MacOs/MachO.hpp
+++ UCodeLang/UCodeLang/Compliation/Back/MacOs/MachO.hpp
@@ -1,5 +1,29 @@
 #pragma once
+#include "UCodeLang/LangCore/UCodeLangNameSpace.hpp"
+#include <UCodeLang/LangCore/BitMaker.hpp>
+#include <UCodeLang/LangCore/LangTypes.hpp>
+UCodeLangStart
+
 class MachO
 {
+public:
+	enum class CPUtype
+	{
+		x86 = 0x00000007,
+		Arm = 0x0000000C,
+	};
+
+
+	void ToBytes(BitMaker& bit) const;
+	bool FromBytes(BitReader& bit);
+
+	static bool FromFile(const Path& path, MachO& file);
+	static bool ToFile(const MachO& file, const Path& path);
+
+	static bool FromBytes(MachO& file, const BytesView Bytes);
+	static BytesPtr ToBytes(const MachO& file);
+private:
 };
+
+UCodeLangEnd
 
