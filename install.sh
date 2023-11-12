ecca496b40990a5492f46fac65e0ab1fac5ab3ff
LostbBlizzard <noodproplayz@gmail.com>
2023-11-12 05:29:33 -0800

    added iinstall.sh for linux

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd "$HOME"
+mkdir -p "$HOME/.ucodelang"
+
+
+echo "To complete installation of 'ucodelang' add the following line to your .bash_profile file:";
+echo ". \"$HOME/.ucodelang/bin\"";
