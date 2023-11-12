f0c186a973533f8760b19f37092d74d8114c82ac
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-11-12 10:17:56 -0800

    added install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,71 @@
 cd "$HOME"
 mkdir -p "$HOME/.ucodelang"
 
+mkdir -p "$HOME/.ucodelang/bin"
 
-echo "To complete installation of 'ucodelang' add the following line to your .bash_profile file:";
-echo ". \"$HOME/.ucodelang/bin\"";
+cd "$HOME/.ucodelang/bin"
+
+if [ "$(uname)" == "Darwin" ]; then
+curl -LJO https://github.com/LostbBlizzard/UCodeLang/releases/download/Release-0.0.5/uclang-macos
+
+mv uclang-macos uclang
+chmod +x uclang
+
+curl -LJO https://github.com/LostbBlizzard/UCodeLang/releases/download/Release-0.0.5/uclanglsp-macos
+
+mv uclanglsp-macos uclanglsp
+
+chmod +x uclanglsp
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+
+curl -LJO https://github.com/LostbBlizzard/UCodeLang/releases/download/Release-0.0.5/uclang-linux64
+
+mv uclang-linux64 uclang
+chmod +x uclang
+
+curl -LJO https://github.com/LostbBlizzard/UCodeLang/releases/download/Release-0.0.5/uclanglsp-linux64
+
+mv uclanglsp-linux64 uclanglsp
+
+chmod +x uclanglsp
+
+echo ""
+
+fi
+
+mkdir -p "$HOME/.ucodelang/modules"
+
+cd "$HOME/.ucodelang/modules"
+
+mkdir ./gitprojecttep/
+git clone https://github.com/LostbBlizzard/UCodeLang ./gitprojecttep
+cp -r ./gitprojecttep/UCodeAPI/StandardLibrary ./
+cp -r ./gitprojecttep/UCodeAPI/NStandardLibrary ./
+
+rm -f -v -r ./gitprojecttep/ 
+
+if ! grep -Fxq "export PATH=$HOME/.ucodelang/bin" $HOME/.bashrc
+then
+ echo "export PATH=$HOME/.ucodelang/bin" >> $HOME/.bashrc
+fi
+
+
+if ! grep -Fxq "export PATH=$HOME/.ucodelang/bin" $HOME/.profile
+then
+ echo "export PATH=$HOME/.ucodelang/bin" >> $HOME/.profile
+fi
+
+if ! grep -Fxq "export PATH=$HOME/.ucodelang/bin" $HOME/.bash_profile
+then
+ echo "export PATH=$HOME/.ucodelang/bin" >> $HOME/.bash_profile
+fi
+
+cd "$HOME/.ucodelang/bin"
+
+./uclang index "$HOME/.ucodelang/modules/StandardLibrary"
+
+./uclang index "$HOME/.ucodelang/modules/NStandardLibrary"
+
+
+echo "installation of 'ucodelang' is complete try using \"uclang -help\"";
