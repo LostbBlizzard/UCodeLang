8313e92faab0a4fa0208558248d70afce045508b
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-11-12 12:51:57 -0800

    added windows install

---
+++ install.bat
@@ -0,0 +1,36 @@
+
+cd %UserProfile%
+mkdir .ucodelang
+
+cd ./.ucodelang
+
+mkdir "bin"
+
+mkdir "modules"
+
+cd ./bin
+
+curl.exe -LJO https://github.com/LostbBlizzard/UCodeLang/releases/download/Release-0.0.5/uclang-win64.exe
+
+ren uclang-win64.exe  uclang.exe
+
+curl.exe -LJO https://github.com/LostbBlizzard/UCodeLang/releases/download/Release-0.0.5/uclanglsp-win64.exe
+
+ren uclanglsp-win64.exe  uclanglsp.exe
+
+cd ./../modules
+
+git clone https://github.com/LostbBlizzard/UCodeLang ./gitprojecttep
+move ./gitprojecttep/UCodeAPI/StandardLibrary ./
+move ./gitprojecttep/UCodeAPI/NStandardLibrary ./
+
+RMDIR /s /q ./gitprojecttep/ 
+
+
+call "%UserProfile%/.ucodelang/bin/uclang" index "%UserProfile%/.ucodelang/modules/StandardLibrary
+
+call "%UserProfile%/.ucodelang/bin/uclang" index "%UserProfile%/.ucodelang/modules/NStandardLibrary
+
+echo "add environment variable %UserProfile%/.ucodelang/bin/uclang to your PATH to complete"
+echo "installation of ucodelang. then try using \"uclang -help\"";
+pause
