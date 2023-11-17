0ab5c2e866e69816ca82baf069ac7584d337d748
LostbBlizzard <noodproplayz@gmail.com>
2023-11-16 17:25:48 -0800

    fixed web stuff

--- UCodeWebsite/content/about.md
+++
@@ -1,5 +0,0 @@
-+++
-title = 'About'
-date = 2023-11-15T16:46:14-08:00
-+++
-[UCodeIDE](/UCodeIDE/UCodeIDE.html)
--- premake5.lua
+++ premake5.lua
@@ -418,7 +418,11 @@ project "UCodeIDE"
       links {"glfw"}
       linkoptions { "-sUSE_GLFW=3","--preload-file wasmassets"}
       postbuildmessage ("copying output to UCodeWebsite")
-      postbuildcommands { "cp -r %{cfg.buildtarget.directory} %{wks.location}/UCodeWebsite/static/UCodeIDE"}
+      postbuildcommands { 
+         "rm -r %{wks.location}/UCodeWebsite/static/UCodeIDE",
+         "cp -f -r %{cfg.buildtarget.directory}/ %{wks.location}/UCodeWebsite/static/UCodeIDE",
+         "cp %{prj.location}/index.html %{wks.location}/UCodeWebsite/static/UCodeIDE/UCodeIDE.html"
+      }
 
    filter { "system:linux","platforms:not Web" }
     kind "ConsoleApp"   
