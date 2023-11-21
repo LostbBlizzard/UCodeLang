02b93dee31c10e79dd7b216f8ff4a2dab7266664
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-11-21 07:04:17 -0800

    added Run Tests And skip in UCodeIDE.

--- UCApp/tests/Test.hpp
+++ UCApp/tests/Test.hpp
@@ -263,9 +263,9 @@ UCodeTestStart
 		size_t End;
 	};
 	static const Vector<SkipTestRange> UCodeVmSkipTests
-		= { {48, Tests.size() } };
+		= { {54, Tests.size() } };
 	static const Vector<SkipTestRange> C89SkipTests
-		= { {48, Tests.size() } };
+		= { {54, Tests.size() } };
 	static const Vector<SkipTestRange> WasmSkipTests
 		= { {4, Tests.size() } };
 
