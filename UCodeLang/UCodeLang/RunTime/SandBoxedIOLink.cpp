497e0f6116448fcb587c0df8ef66e1879b0b47a7
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-12-10 18:48:56 -0800

    updated native print

--- UCodeLang/UCodeLang/RunTime/SandBoxedIOLink.cpp
+++ UCodeLang/UCodeLang/RunTime/SandBoxedIOLink.cpp
@@ -1,10 +1,34 @@
 #pragma once
 #include "SandBoxedIOLink.hpp"
+#include "Interpreters/Interpreter.hpp"
 UCodeLangStart
 
-void SandBoxedIOLink::Link(RunTimeLib* lib)
+
+template<typename T, typename... Pars>
+using NativeCall = RunTimeLib::NativeCall<T, Pars...>;
+
+void SandBoxedIOLink::Link(RunTimeLib* lib, const LinkSettings& settings = {})
 {
 
+	if (settings.AddCInAndOut)
+	{
+		lib->Add_CPPCall("putchar", [](InterpreterCPPinterface& Input)
+		{
+			int Par0 = Input.GetParameter<int>();
+			Input.Set_Return(putchar(Par0));
+		},putchar);
+
+		lib->Add_CPPCall("putspan", [](InterpreterCPPinterface& Input)
+			{
+				auto Par0 = Input.GetParameter<const char*>();
+				auto Par1 = Input.GetParameter<size_t>();
+
+				Input.Set_Return(fwrite(Par0, 1, Par1, stdout));
+			}, (NativeCall<size_t, const char*, size_t>)[](const char* pointer, size_t size)
+			{
+				return fwrite(pointer, 1, size, stdout);
+			});
+	}
 }
 
 UCodeLangEnd
