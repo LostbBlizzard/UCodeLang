f1669e41529d0fa7d2eb019f82b6be4b4c6380d7
LostbBlizzard <noodproplayz@gmail.com>
2024-01-10 03:38:06 -0800

    Started updateing DeferStatement

--- UCodeLang/UCodeLang/Compilation/Front/SystematicAnalysis/Statements/DeferStatement.cpp
+++ UCodeLang/UCodeLang/Compilation/Front/SystematicAnalysis/Statements/DeferStatement.cpp
@@ -2,10 +2,19 @@
 #include "UCodeLang/Compilation/Front/SystematicAnalysis.hpp"
 UCodeLangFrontStart
 
+//how defer is as a Lambda that just so happened to have its destructor have the statements
 void SystematicAnalysis::OnDeferStatement(const DeferStatementNode& node)
 {
+	const String LambdaName = CompilerGenerated("DeferLambda") + std::to_string((uintptr_t)&node);
+
+
 	if (_PassType == PassType::GetTypes)
 	{
+		auto& LambdaSym = Symbol_AddSymbol(SymbolType::LambdaObject, LambdaName, _Table._Scope.ThisScope, AccessModifierType::Public);
+		_Table.AddSymbolID(LambdaSym, Symbol_GetSymbolID(node));
+		LambdaSym.Info = Unique_ptr<LambdaInfo>(new LambdaInfo());
+
+
 		for (auto& Item : node._Base._Nodes)
 		{
 			if (Item->Get_Type() == NodeType::RetStatementNode)
@@ -23,13 +32,40 @@ void SystematicAnalysis::OnDeferStatement(const DeferStatementNode& node)
 	}
 	else if (_PassType == PassType::FixedTypes)
 	{
+		auto& LambdaSym = *Symbol_GetSymbol(Symbol_GetSymbolID(node));
+		LambdaInfo* Info = LambdaSym.Get_Info<LambdaInfo>();
+
+		{
+			VarableUseData Data;
+			_Varable.push(std::move(Data));
+		}
+
 		for (auto& Item : node._Base._Nodes)
 		{
 			OnStatement(*Item);
 		}
+
+		{
+			VarableUseData& UseData = _Varable.top();
+
+			for (auto& Item : UseData._UsedSymbols)
+			{
+				LambdaFieldInfo NewValue;
+				NewValue.Name = ScopeHelper::GetNameFromFullName(Item->FullName);
+				NewValue.Type = Item->VarType;
+				NewValue.Sym = Item;
+
+				Info->_CapturedVarables.push_back(std::move(NewValue));
+			}
+
+			int a = 10;
+		}
 	}
 	else if (_PassType == PassType::BuidCode)
 	{
+		auto& LambdaSym = *Symbol_GetSymbol(Symbol_GetSymbolID(node));
+		LambdaInfo* Info = LambdaSym.Get_Info<LambdaInfo>();
+
 		for (auto& Item : node._Base._Nodes)
 		{
 			OnStatement(*Item);
