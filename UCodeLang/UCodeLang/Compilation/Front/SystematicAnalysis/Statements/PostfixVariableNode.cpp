d3dd446f0fad7ba8021fc2677932f173c29f1ba0
LostbBlizzard <noodproplayz@gmail.com>
2024-01-21 13:11:24 -0800

    fixed postfix BuidCode _LookingForTypes

--- UCodeLang/UCodeLang/Compilation/Front/SystematicAnalysis/Statements/PostfixVariableNode.cpp
+++ UCodeLang/UCodeLang/Compilation/Front/SystematicAnalysis/Statements/PostfixVariableNode.cpp
@@ -8,69 +8,69 @@ void SystematicAnalysis::OnPostfixVariableNode(const PostfixVariableNode& node)
 	{
 		OnExpressionTypeNode(node._ToAssign._Value.get(), GetValueMode::ReadAndWrite);
 	}
-	else
-		if (_PassType == PassType::FixedTypes)
+	else if (_PassType == PassType::FixedTypes)
+	{
+		_LookingForTypes.push(TypesEnum::Any);
+		OnExpressionTypeNode(node._ToAssign._Value.get(), GetValueMode::ReadAndWrite);
+		_LookingForTypes.pop();
+
+		auto ExType = _LastExpressionType;
+
+		auto HasInfo = Type_HasPostfixOverLoadWith(ExType, node._PostfixOp->Type);
+		if (!HasInfo.HasValue)
 		{
-			_LookingForTypes.push(TypesEnum::Any);
+			LogError_CantFindPostfixOpForTypes(NeverNullptr(node._PostfixOp), ExType);
+		}
+
+
+		PostFixExpressionNode_Data t;
+
+		if (HasInfo.Value)
+		{
+			FuncInfo* f = HasInfo.Value.value()->Get_Info<FuncInfo>();
+			t.FuncToCall = HasInfo.Value.value();
+			t.Op0 = f->Pars[0].Type;
+		}
+		else
+		{
+			t.Op0 = _LastExpressionType;
+		}
+
+		_PostFix_Datas.AddValue(Symbol_GetSymbolID(node), t);
+	}
+	else if (_PassType == PassType::BuidCode)
+	{
+		const PostFixExpressionNode_Data& Data = _PostFix_Datas.GetValue(Symbol_GetSymbolID(node));
+
+		if (Data.FuncToCall)
+		{
+			FuncInfo* f = Data.FuncToCall->Get_Info<FuncInfo>();
+
+			Get_FuncInfo V;
+			V.Func = f;
+			V.SymFunc = Data.FuncToCall;
+			V.ThisPar = Get_FuncInfo::ThisPar_t::NoThisPar;
+
+			ScopedNameNode Tep;
+
+			ValueParametersNode pars;
+			pars._Nodes.push_back(Unique_ptr<Node>(node._ToAssign._Value.get()));
+
+			IR_Build_FuncCall(V, Tep, pars);
+
+			pars._Nodes[0].release();//its ok Tree has pointer.
+
+			_LastExpressionType = V.Func->Ret;
+		}
+		else
+		{
+			_LookingForTypes.push(Data.Op0);
 			OnExpressionTypeNode(node._ToAssign._Value.get(), GetValueMode::ReadAndWrite);
 			_LookingForTypes.pop();
 
-			auto ExType = _LastExpressionType;
+			IRInstruction* LoadV = _IR_LastExpressionField;
 
-			auto HasInfo = Type_HasPostfixOverLoadWith(ExType, node._PostfixOp->Type);
-			if (!HasInfo.HasValue)
-			{
-				LogError_CantFindPostfixOpForTypes(NeverNullptr(node._PostfixOp), ExType);
-			}
-
-
-			PostFixExpressionNode_Data t;
-
-			if (HasInfo.Value)
-			{
-				FuncInfo* f = HasInfo.Value.value()->Get_Info<FuncInfo>();
-				t.FuncToCall = HasInfo.Value.value();
-				t.Op0 = f->Pars[0].Type;
-			}
-			else
-			{
-				t.Op0 = _LastExpressionType;
-			}
-
-			_PostFix_Datas.AddValue(Symbol_GetSymbolID(node), t);
-		}
-		else
-			if (_PassType == PassType::BuidCode)
-			{
-				const PostFixExpressionNode_Data& Data = _PostFix_Datas.GetValue(Symbol_GetSymbolID(node));
-
-				if (Data.FuncToCall)
-				{
-					FuncInfo* f = Data.FuncToCall->Get_Info<FuncInfo>();
-
-					Get_FuncInfo V;
-					V.Func = f;
-					V.SymFunc = Data.FuncToCall;
-					V.ThisPar = Get_FuncInfo::ThisPar_t::NoThisPar;
-
-					ScopedNameNode Tep;
-
-					ValueParametersNode pars;
-					pars._Nodes.push_back(Unique_ptr<Node>(node._ToAssign._Value.get()));
-
-					IR_Build_FuncCall(V, Tep, pars);
-
-					pars._Nodes[0].release();//its ok Tree has pointer.
-
-					_LastExpressionType = V.Func->Ret;
-				}
-				else
-				{
-					OnExpressionTypeNode(node._ToAssign._Value.get(), GetValueMode::ReadAndWrite);
-
-					IRInstruction* LoadV = _IR_LastExpressionField;
-
-					auto Type = _LastExpressionType;
+			auto Type = _LastExpressionType;
 
 #define buildPortFixU(x)\
 		if (node._PostfixOp->Type == TokenType::increment)\
@@ -87,79 +87,79 @@ void SystematicAnalysis::OnPostfixVariableNode(const PostfixVariableNode& node)
 #define buildPortFixS(x) buildPortFixU(x)
 
 
-					Debug_Add_SetLineNumber(NeverNullptr(node._PostfixOp), _IR_LookingAtIRBlock->Instructions.size());
+			Debug_Add_SetLineNumber(NeverNullptr(node._PostfixOp), _IR_LookingAtIRBlock->Instructions.size());
 
-					switch (Type._Type)
-					{
-					case TypesEnum::uInt8:
-					{
-						buildPortFixU(8);
-					}
-					break;
+			switch (Type._Type)
+			{
+			case TypesEnum::uInt8:
+			{
+				buildPortFixU(8);
+			}
+			break;
 
-					case TypesEnum::uInt16:
-					{
-						buildPortFixU(16);
-					}
-					break;
-					case TypesEnum::uInt32:
-					{
-						buildPortFixU(32);
-					}
-					break;
-					case TypesEnum::uInt64:
-					{
-						buildPortFixU(64);
-					}
-					break;
-					case TypesEnum::uIntPtr:
-					{
-						if (_Settings->PtrSize == IntSizes::Int64) {
-							buildPortFixU(64)
-						}
-						else
-						{
-							buildPortFixU(32)
-						}
-					}
-					break;
-					case TypesEnum::sInt8:
-					{
-						buildPortFixS(8);
-					}
-					break;
-					case TypesEnum::sInt16:
-					{
-						buildPortFixS(16);
-					}
-					break;
-					case TypesEnum::sInt32:
-					{
-						buildPortFixS(32);
-					}
-					break;
-					case TypesEnum::sInt64:
-					{
-						buildPortFixS(64);
-					}
-					break;
-					case TypesEnum::sIntPtr:
-					{
-						if (_Settings->PtrSize == IntSizes::Int64) {
-							buildPortFixS(64)
-						}
-						else
-						{
-							buildPortFixS(32)
-						}
-					}
-					break;
-					default:
-						break;
-					}
-					IR_WriteTo(_IR_LastExpressionField, Type, _IR_LastStoreField);
+			case TypesEnum::uInt16:
+			{
+				buildPortFixU(16);
+			}
+			break;
+			case TypesEnum::uInt32:
+			{
+				buildPortFixU(32);
+			}
+			break;
+			case TypesEnum::uInt64:
+			{
+				buildPortFixU(64);
+			}
+			break;
+			case TypesEnum::uIntPtr:
+			{
+				if (_Settings->PtrSize == IntSizes::Int64) {
+					buildPortFixU(64)
+				}
+				else
+				{
+					buildPortFixU(32)
 				}
 			}
+			break;
+			case TypesEnum::sInt8:
+			{
+				buildPortFixS(8);
+			}
+			break;
+			case TypesEnum::sInt16:
+			{
+				buildPortFixS(16);
+			}
+			break;
+			case TypesEnum::sInt32:
+			{
+				buildPortFixS(32);
+			}
+			break;
+			case TypesEnum::sInt64:
+			{
+				buildPortFixS(64);
+			}
+			break;
+			case TypesEnum::sIntPtr:
+			{
+				if (_Settings->PtrSize == IntSizes::Int64) {
+					buildPortFixS(64)
+				}
+				else
+				{
+					buildPortFixS(32)
+				}
+			}
+			break;
+			default:
+				break;
+			}
+			IR_WriteTo(_IR_LastExpressionField, Type, _IR_LastStoreField);
+		}
+	}
 }
 
 
