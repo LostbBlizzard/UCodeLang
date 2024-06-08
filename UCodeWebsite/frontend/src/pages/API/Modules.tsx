ed20679c1ea5f2f9f50c978123f8eeee77f4a87f
LostbBlizzard <noodproplayz@gmail.com>
2024-06-07 21:37:45 -0700

    Remove: UCodeWebsite

--- UCodeWebsite/frontend/src/pages/API/Modules.tsx
+++
@@ -1,8 +0,0 @@
-interface Get_Modules_Module {
-  id: number;
-  module_name: string;
-}
-
-interface Get_Module {
-  module_name: string;
-}
--- UCodeWebsite/frontend/src/pages/SignUp.tsx
+++
@@ -1,9 +0,0 @@
-export default function SignUp() {
-  return (
-    <>
-      <input type="text" placeholder="UserName.."></input>
-      <button type="button">Use GitHub To LogIn</button>
-      <button type="button">Use Google To LogIn</button>
-    </>
-  );
-}
